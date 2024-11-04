ea5d51dcd2390f64da2776b7450e84fd4c3f8d94
ecaleb97 <valley_rob.0c@icloud.com>
2024-11-04 17:25:35 +0100

    add Level 3

---
+++ Level-3/add_prime_sum/add_prime_sum.c
@@ -0,0 +1,83 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   add_prime_sum.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: envillan <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/31 07:28:30 by envillan          #+#    #+#             */
+/*   Updated: 2024/10/31 07:43:41 by envillan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_atoi(char *str)
+{
+	int	result;
+	int	sign;
+	int	i;
+
+	i = 0;
+	result = 0;
+	sign = 1;
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == '-')
+		sign = -1;
+	if (str[i] == '+' || str[i] == '-')
+		i++;
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + str[i] - 48;
+		i++;
+	}
+	return (sign * result);
+}
+
+int	is_prime(int num)
+{
+	int	i;
+
+	i = 2;
+	if (num <= 1)
+		return (0);
+	while (i * i <= num)
+	{
+		if (num % i == 0)
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+void	put_nbr(int n)
+{
+	char	digit;
+
+	if (n >= 10)
+		put_nbr(n / 10);
+	digit = n % 10 + '0';
+	write(1, &digit, 1);
+}
+
+int	main(int argc, char **argv)
+{
+	int	n;
+	int	sum;
+
+	if (argc == 2)
+	{
+		n = ft_atoi(argv[1]);
+		sum = 0;
+		while (n > 0)
+		{
+			if (is_prime(n))
+				sum += n;
+			n--;
+		}
+		put_nbr(sum);
+	}
+	write(1, "\n", 1);
+	return (0);
+}

077d9eb486d0f3fbc8f35684310499f518ae4399
ecaleb97 <valley_rob.0c@icloud.com>
2024-11-15 23:36:56 +0100

    add paramsum

--- Level-4/paramsum/paramsum.c
+++ Level-4/paramsum/paramsum.c
@@ -6,7 +6,7 @@
 /*   By: envillan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/15 23:17:21 by envillan          #+#    #+#             */
-/*   Updated: 2024/11/15 23:20:23 by envillan         ###   ########.fr       */
+/*   Updated: 2024/11/15 23:36:15 by envillan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,20 +17,50 @@ int	is_space(char c)
 	return (c == ' ' || (c >= 9 && c <= 13));
 }
 
-int	ft_atoi(char *str)
+void	ft_putchar(char c)
 {
-	int	result;
-	int	i;
+	write(1, &c, 1);
+}
 
-	result = 0;
+// int	ft_atoi(char *str)
+// {
+// 	int	result;
+// 	int	i;
+// 	int	sign;
+
+// 	sign = 1;
+// 	result = 0;
+// 	i = 0;
+// 	while (is_space(str[i]))
+// 		i++;
+// 	if (str[i] == '-')
+// 		sign = -1;
+// 	if (str[i] == '-' || str[i] == '+')
+// 		i++;
+// 	while (str[i] >= '0' && str[i] <= '9')
+// 	{
+// 		result = result * 10 + str[i] - '0';
+// 		i++;
+// 	}
+// 	return (sign * result);
+// }
+
+void	ft_putnbr(int number)
+{
+	if (number > 9)
+	{
+		ft_putnbr(number / 10);
+		ft_putnbr(number % 10);
+	}
+	else
+		ft_putchar(number + '0');
 }
 
 int	main(int argc, char **argv)
 {
-	int	i;
-
-	i = 0;
-	while (argc)
-		i++;
-	return (0)
+	// int	i;
+	(void)argv;
+	ft_putnbr(argc - 1);
+	ft_putchar('\n');
+	return (0);
 }
